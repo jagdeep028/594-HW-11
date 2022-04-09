7419dda6e2927caed4eaaf5b65160183c2e9aa77
jagdee028 <jagdeep820@gmail.com>
2022-04-09 01:31:31 -0400

    Update Saturday Morning

--- src/edu/upenn/cit594/Main.java
+++ src/edu/upenn/cit594/Main.java
@@ -5,6 +5,7 @@ import edu.upenn.cit594.datamanagement.ReadTweetTXT;
 import edu.upenn.cit594.datamanagement.StateReader;
 import edu.upenn.cit594.datamanagement.TweetReader;
 import edu.upenn.cit594.processor.FluTweetProcessor;
+import edu.upenn.cit594.processor.StateTweetProcessor;
 import edu.upenn.cit594.util.State;
 import edu.upenn.cit594.util.Tweet;
 
@@ -15,23 +16,14 @@ public class Main {
     public static void main(String[] args){
 
         StateReader sReader = new ReadStateCSV("states.csv");
-        List<State> states = sReader.readStateFile();
-
-        for (State state : states) {
-            System.out.println(state.getState());
-            System.out.println(state.getLatitude());
-            System.out.println(state.getLongitude());
-        }
-
         TweetReader tReader = new ReadTweetTXT("flu_tweets.txt");
-        FluTweetProcessor fluProcessor = new FluTweetProcessor(tReader);
 
-        List <Tweet> tweets = fluProcessor.findFluTweets();
+        StateTweetProcessor stateProcessor = new StateTweetProcessor(sReader,tReader);
+        HashMap<Tweet, State> fluMap = stateProcessor.nearestState();
 
-        for (Tweet tweet: tweets){
-            System.out.println(tweet.getLatitude());
-            System.out.println(tweet.getLongitude());
-            System.out.println(tweet.getText());
+        for (Tweet key: fluMap.keySet()){
+            System.out.println(key.getText());
+            System.out.println(fluMap.get(key).getState());
         }
     }
 
--- src/edu/upenn/cit594/processor/StateTweetProcessor.java
+++ src/edu/upenn/cit594/processor/StateTweetProcessor.java
@@ -1,4 +1,53 @@
 package edu.upenn.cit594.processor;
 
+import edu.upenn.cit594.datamanagement.StateReader;
+import edu.upenn.cit594.datamanagement.TweetReader;
+import edu.upenn.cit594.util.State;
+import edu.upenn.cit594.util.Tweet;
+
+import java.util.*;
+
 public class StateTweetProcessor {
+    protected StateReader sReader;
+    protected TweetReader tReader;
+    protected List<Tweet> tweets;
+    protected List<State> states;
+
+    public StateTweetProcessor(StateReader sReader, TweetReader tReader){
+        this.sReader = sReader;
+        this.tReader = tReader;
+
+        tweets = new FluTweetProcessor(this.tReader).findFluTweets();
+        states = sReader.readStateFile();
+    }
+
+
+    public HashMap<Tweet, State> nearestState(){
+
+        HashMap<Tweet, State> fluMap = new HashMap<>();
+        HashMap<State, Double> allDistances = new HashMap<>();
+        double distance;
+
+        for(Tweet tweet: tweets){
+
+            for(State state: states){
+                distance = Math.sqrt((state.getLongitude()-tweet.getLongitude())+(state.getLatitude()- tweet.getLatitude()));
+                allDistances.put(state,distance);
+            }
+
+            Double minDistance = Collections.min(allDistances.values());
+
+            for(State key: allDistances.keySet()){
+                if (minDistance.equals(allDistances.get(key))){
+                    fluMap.put(tweet,key);
+                }
+            }
+
+        }
+
+        return fluMap;
+    }
+
+
+
 }
